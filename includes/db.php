c41e9b7a39a016f9d2b1b521563285243b945499
djwood <djwood@mtu.edu>
2017-10-09 11:46:52 -0400

    testing

--- includes/db.php
+++ includes/db.php
@@ -4,6 +4,7 @@
 		static $con; // con = connection
 
 		if(!isset($con)) {
+			if(!file_exists("../private/config.ini")) echo "config.ini not found.";
 			$config = parse_ini_file("../private/config.ini");
 			$con = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
 		}
