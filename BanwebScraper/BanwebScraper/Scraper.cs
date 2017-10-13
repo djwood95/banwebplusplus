b832048ab0690534f76ece27292f43eec515b4ab
blakexiaolong <blakexiaolong36@gmail.com>
2017-10-13 12:58:27 -0400

    Added MySql command support

--- BanwebScraper/BanwebScraper/Scraper.cs
+++ BanwebScraper/BanwebScraper/Scraper.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
-
+using MySql.Data;
+using System.Data;
 
 namespace BanwebScraper
 {
@@ -17,6 +18,48 @@ namespace BanwebScraper
         {
             doc = new HtmlDocument();
             doc.Load(filepath);
+        }
+
+        private static DataTable PushToDb(MySqlCommand command)
+        {
+            DataTable dt = new DataTable();
+            using (var connection = new MySqlConnection())
+            {
+                try
+                {
+                    command.Connection = connection;
+                    dt.Load(command.ExecuteReader);
+                }
+                catch (Exception e)
+                {
+                    Console.Write($"Exception Caught: {e}");
+                }
+            }
+            return dt;
+        }
+        private static void PushToDb(MySqlCommand[] commands)
+        {
+            using (var connection = new MySqlConnection)
+            {
+                using (var transaction = new MySqlTransaction)
+                {
+                    try
+                    {
+                        transaction.Begin();
+                        foreach (var command in commands)
+                        {
+                            command.Connection = connection;
+                            command.Transacation = transaction;
+                            command.ExecuteNonQuery();
+                        }
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Write($"Exception Caught: {e}");
+                    }
+                }
+            }
         }
     }
 }
