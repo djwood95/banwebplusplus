dc98c42b1ac6e716bc6c86c3821babc4268e68f5
Alexander Eschenauer <alexandereschenauer@rover-221-162.rovernet.mtu.edu>
2017-11-02 18:58:42 -0400

    make scheuduling greatd again

--- view/index.php
+++ view/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="shortcut icon" href="DonaldTrump.ico"/>
+    <title>MAKE SCHEDULING GREAT AGAIN</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
