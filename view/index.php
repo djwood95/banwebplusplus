8f0b3b58e5deee43b7e448209b49efce9f423a44
Alexander Eschenauer <alexandereschenauer@rover-221-162.rovernet.mtu.edu>
2017-11-02 19:12:07 -0400

    authenticate bby maybe idk wtf

--- view/index.php
+++ view/index.php
@@ -120,6 +120,17 @@
   <meta name="google-signin-client_id" content="1079673860784-13aa7jbs3nrmo7t3j5pqk75lu795elec.apps.googleusercontent.com">
 
     <script> function onSignIn(googleUser) {
+
+      var id_token = googleUser.getAuthResponse().id_token;
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://yourbackend.example.com/tokensignin');
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.onload = function() {
+        console.log('Signed in as: ' + xhr.responseText);
+      };
+      xhr.send('idtoken=' + id_token);
+
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
