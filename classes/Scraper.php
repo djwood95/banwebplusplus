10d142949530501bf6e22bde6e682fd671c5369c
djwood <djwood@mtu.edu>
2018-01-14 22:15:01 -0500

    testing

--- classes/Scraper.php
+++ classes/Scraper.php
@@ -6,10 +6,15 @@ use DiDom\Document;
 
 class Scraper extends Mapper {
 	
+	private $courseDescriptions;
+
 	public function updateSections($subject, $mode) {
 
 		$semesterCodeList = self::getAvailableSemesters();
 		$startMemory = memory_get_usage();
+
+		if($mode == "detailed") $this->courseDescriptions = self::getCourseDescriptions();
+		//print_r($this->courseDescriptions);
 
 		foreach($semesterCodeList as $semesterCode) {
 			self::scrapeSemester($semesterCode, $subject, $mode);
@@ -83,7 +88,8 @@ class Scraper extends Mapper {
 
 				$data = array();
 				//Get the basics that need frequent updating
-				$data['crn'] = trim($cols[0]->find('a')[0]->text());
+				$crn = trim($cols[0]->find('a')[0]->text());
+				$data['crn'] = $crn;
 				$subj = trim($cols[1]->text());
 				$crse = trim($cols[2]->text());
 				$data['courseNum'] = $subj." ".$crse;
@@ -103,11 +109,24 @@ class Scraper extends Mapper {
 					$data['dates'] = trim($cols[13]->text());
 					$data['fee'] = trim($cols[15]->text());
 
-					$extraInfo = self::getExtraInfo($semesterCode, $subj, $crse, $crn);
-					$data = array_merge($data, $extraInfo);
+					// If course description has not been retrieved, get it from banweb
+					echo $data['courseNum'];
+					if(isset($this->courseDescriptions[$data['courseNum']])) {
+						$extraInfo = $this->courseDescriptions[$data['courseNum']];
+						print_r($extraInfo);
+						echo "<br/>";
+						$data = array_merge($data, $extraInfo);
+					} else {
+						$extraInfo = self::getExtraInfo($semesterCode, $subj, $crse, $crn);
+						print_r($extraInfo);
+						echo "<br/>";
+						$data = array_merge($data, $extraInfo);
+						echo " ";
+					}
+			
 				}
 
-				print_r($data);
+				//print_r($data);
 			}
 
 		}
@@ -115,44 +134,15 @@ class Scraper extends Mapper {
 	}
 
 	private function getExtraInfo($semesterCode, $subject, $courseNum, $crn) {
-		$curl = curl_init();
 
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://www.banweb.mtu.edu/pls/owa/bwckschd.p_disp_listcrse?term_in=$semesterCode&subj_in=$subject&crse_in=$courseNum&crn_in=$crn",
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => "",
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 30,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
-		  CURLOPT_HTTPHEADER => array(
-		    "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-		    "accept-encoding: gzip, deflate, br",
-		    "accept-language: en-US,en;q=0.8",
-		    "cache-control: no-cache",
-		    "cookie: TESTID=set; MTUSTUDENT=set; __cfduid=d26e6bb2d71946ec2c6bb2e8aab9009ba1493084278; __unam=7bc3574-15d17e1c11e-7ed8a39b-1; _ceg.s=owwpq3; _ceg.u=owwpq3; _ceir=1; MTUPHPBB_data=a%3A2%3A%7Bs%3A11%3A%22autologinid%22%3Bs%3A0%3A%22%22%3Bs%3A6%3A%22userid%22%3Bs%3A5%3A%2219847%22%3B%7D; MTUPHPBB_sid=4739afde565c0abc3142e85180223c20; MTUPHPBB_t=a%3A4%3A%7Bi%3A63325%3Bi%3A1506972700%3Bi%3A63294%3Bi%3A1506972724%3Bi%3A63318%3Bi%3A1506972744%3Bi%3A63336%3Bi%3A1506972796%3B%7D; _ga=GA1.2.2134920387.1493136985; auditCookie=\"M/48rcrRqLJvUBBXLLcAEAOFd/CrqnQt3X0c1F165PlzyPcGcGyolrhJBoHELnyYyAKi4paZbnYWllDdMpTkiny3jxjXkkJyLLtpS8uZ5lY=\"; sghe_magellan_M75783634_locale=en_US; __utma=52490228.2134920387.1493136985.1507738703.1507749784.68; __utmc=52490228; __utmz=52490228.1507749784.68.29.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); sghe_magellan_null_username=; sghe_magellan_null_locale=en_US",
-		    "postman-token: 4a0fbea9-40dd-ca1a-c0df-981e32016396",
-		    "referer: http://www.mtu.edu/registrar/students/registration/prepare/",
-		    "upgrade-insecure-requests: 1",
-		    "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-		  ),
-		));
-
-		$response = curl_exec($curl);
-		$err = curl_error($curl);
-
-		curl_close($curl);
-
-		if ($err) {
-			die("cURL Error #:" . $err);
-		}
+		$html = self::getHtmlFromUrl("https://www.banweb.mtu.edu/pls/owa/bwckschd.p_disp_listcrse?term_in=$semesterCode&subj_in=$subject&crse_in=$courseNum&crn_in=$crn");
 
 		// Stop if no info found to prevent errors later on
-		if(strpos($response, "No classes were found that meet your search criteria") !== false) {
+		if(strpos($html, "No classes were found that meet your search criteria") !== false) {
 			return null;
 		}
 
-		$dom = HtmlDomParser::str_get_html( $response );
+		$dom = HtmlDomParser::str_get_html( $html );
 		$dataTable = $dom->find('table.datadisplaytable')[0];
 		$html = $dataTable->find('tr',0)->find('td',0);
 
@@ -208,13 +198,41 @@ class Scraper extends Mapper {
 		return $data;
 	}
 
-	private function getAvailableSemesters() {
-		$currentYear = date("Y");
+	private function getCourseDescriptions() {
+		$html = self::getHtmlFromUrl("https://www.banweb.mtu.edu/pls/owa/stu_ctg_utils.p_online_all_courses_ug");
+		$sections = explode("<hr>", $html);
+		$courseDescriptions = array();
 
+		$courses = explode("<br><br>", $html);
+		//echo count($courses)." ";
+		foreach($courses as $courseHtml) {
+			preg_match("/<b>([A-Z]{2,3} [0-9]{4}) - ([\S\s]+?)<\/b><br>([\S\s]+?)<br><b>Credits:<\/b>([\S\s]+?)<br>\n<b>Lec-Rec-Lab:<\/b> \(([0-9])-([0-9])-([0-9])\)<br>\n<b>Semesters Offered:<\/b>([\S\s]+?)<br>/", $courseHtml, $matches);
+			$courseNum = $matches[1];
+			//echo $courseNum." ";
+			$courseDescriptions[$courseNum] = [
+				'title' => $matches[2],
+				'description' => $matches[3],
+				'lecCredits' => $matches[5],
+				'recCredits' => $matches[6],
+				'labCredits' => $matches[7],
+				'semestersOffered' => $matches[8]
+			];
+
+			preg_match("/<b>Pre-Requisite\(s\):<\/b> ([\S\s]+?)<br>/", $courseHtml, $matches);
+			$courseDescriptions[$courseNum]['preReqs'] = $matches[1];
+
+			preg_match("/<b>Restrictions: <\/b>\n([\S\s]+?)<br>/", $courseHtml, $matches);
+			$courseDescriptions[$courseNum]['restrictions'] = $matches[1];
+		}
+
+		return $courseDescriptions;
+	}
+
+	private function getHtmlFromUrl($url) {
 		$curl = curl_init();
 
 		curl_setopt_array($curl, array(
-		  CURLOPT_URL => "https://www.banweb.mtu.edu/pls/owa/bzskfcls.p_sel_crse_search",
+		  CURLOPT_URL => "$url",
 		  CURLOPT_RETURNTRANSFER => true,
 		  CURLOPT_ENCODING => "",
 		  CURLOPT_MAXREDIRS => 10,
@@ -240,11 +258,18 @@ class Scraper extends Mapper {
 		curl_close($curl);
 
 		if ($err) {
-			die("cURL Error #:" . $err);
+			return "cURL Error #:" . $err;
 		}
-		
 
-		$dom = HtmlDomParser::str_get_html( $response );
+		return $response;
+	}
+
+	private function getAvailableSemesters() {
+		$currentYear = date("Y");
+
+		$html = self::getHtmlFromUrl("https://www.banweb.mtu.edu/pls/owa/bzskfcls.p_sel_crse_search");
+
+		$dom = HtmlDomParser::str_get_html( $html );
 		$semesterList = $dom->find('#term_input_id')[0]->find('option');
 		$earliestYear = date("Y");
 		foreach($semesterList as $semesterElement) {
@@ -259,15 +284,6 @@ class Scraper extends Mapper {
 		}
 
 		return $semesterCodeList;
-	}
-
-
-	/* Get descriptions and pre-req/co-req info and dump into file */
-	public function getCourseDescriptions() {
-		$html = file_get_contents("https://www.banweb.mtu.edu/pls/owa/stu_ctg_utils.p_online_all_courses_ug");
-		$file = fopen(__DIR__ . "/../banwebFiles/descriptions.html", 'w');
-        fwrite($file, $html);
-        fclose($file);
 	}
 
 }
